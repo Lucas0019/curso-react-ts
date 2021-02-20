ed98146762357dc8da7bf86bf5837a86c0ce04cc
Lucas <lxavier.00@outlook.com>
2021-02-20 00:33:24 -0300

    Entendendo conceito de estado e useEffect

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,25 @@
-import Button from './components/Button'
+//Retorna um array de duas posições
+import { useState } from 'react';
 
 function App() {
-  return (
+
+  // Desestruturação
+  const [contador, setContador]  = useState(0);
+
+  //Aqui nesse caso ele levará 2 segundos para atualizar o contador
+  //com a quantidade de clicks efetuadas anteriomente 
+   function mudaContador() {
+    setTimeout(() => {
+      //OldContador é o estado da aplaicação anterior ao click
+      setContador((oldContador) => oldContador + 5);
+    }, 2000);
+  }
+
+   return (
     <div>
       <h1>Eae Dev, sou um componente</h1>
-      {/* <Button text="Cadastrar"/>  */}
-      <Button>Cadastrar</Button>
+      Contador: {contador}
+      <button onClick={mudaContador}>Aumenta contador</button>
     </div>
   );
 }
